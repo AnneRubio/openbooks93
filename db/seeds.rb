c459914076e61a628f920ced8bc151ed8e0d0f4e
anne rubio <rubio.anne@gmail.com>
2022-12-19 19:04:59 +0100

    added view book index and started the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+puts "cleaning db"
+Book.destroy_all
+Reservation.destroy_all
+
+puts 'cleaning user'
+User.destroy_all
+
+puts 'creating user'
+
+puts 'creating user'
+user1 = User.create(
+  username: "Ana",
+  email: "anne@openbooks93.me",
+  password: "1234567",
+  city: "Montreuil",
+  image_url: "https://avatars.githubusercontent.com/u/109987907?v=4"
+)
+
+user2 = User.create(
+  username: "Fabi",
+  email: "fabio@openbooks93.me",
+  password: "1234567",
+  city: "Saint-Maur",
+  image_url: "https://avatars.githubusercontent.com/u/109987907?v=4"
+)
+
+users = [user1, user2, user3]
+
+
+puts 'creating book'
